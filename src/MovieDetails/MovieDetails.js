18d605a3994a67b98dcfbbb860aa5515bfab2b95
Natasha Vasquez <nvasquez9020@my.pmi.edu>
2025-02-17 18:37:57 -0600

    Able to render data from movie_details file, as well as return back to moviescontainer view, styling added to render based on spec

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -1,9 +1,26 @@
 import './MovieDetails.css';
 
-function MovieDetails() {
+function MovieDetails( {movie} ) {
+  console.log(movie)
+
+  if (!movie) {
+    return <p>Loading...</p>; 
+  }  
+
+  const genres = movie.genre_ids || [];
+
   return (
     <section className='MovieDetails'>
-      <p>Movie Details go here!</p>
+      <img className='BackdropImg' src={movie.backdrop_path} alt={`${movie.title} backdrop`} />
+      <div className='details-container'>
+        <h2>{movie.title}</h2>
+        <div className="genres">
+          {genres.map((genre, index) => (
+            <span key={index} className="genre">{genre}</span>
+          ))}
+        </div>
+        <p>{movie.overview}</p>
+      </div>
     </section>
   );
 }
--- src/MoviesContainer/MoviesContainer.js
+++ src/MoviesContainer/MoviesContainer.js
@@ -1,11 +1,16 @@
 import './MoviesContainer.css';
 import MoviePoster from '../MoviePoster/MoviePoster'
+// import movieDetails from '../data/movie_details';
 
-function MoviesContainer({movies, votingChange }) {
+function MoviesContainer({movies, votingChange, setSelectedMovie }) {
   return (
       <section className='MoviesContainer'>
       {movies.map((movie) => (
-        <MoviePoster key={movie.id} movie={movie} votingChange={votingChange}/>
+        <MoviePoster 
+        key={movie.id} 
+        movie={movie} 
+        votingChange={votingChange}
+        setSelectedMovie={setSelectedMovie} />
       ))}
       </section>
   );
